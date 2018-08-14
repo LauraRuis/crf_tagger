4216d41695607422e7a64f62600d19bf0d07cad1
LauraRuis <lauraruis@live.nl>
2018-08-14 12:55:07 +0200

    remove loop over tagset

--- model.py
+++ model.py
@@ -39,46 +39,40 @@ class BiLSTM_ChainCRF(nn.Module):
         return (torch.randn(2, 1, self.hidden_dim // 2),
                 torch.randn(2, 1, self.hidden_dim // 2))
 
-    def _forward_alg(self, feats):
+    def _forward_belief_prop(self, feats):
 
         # initialize the recursion variables
         init_alphas = torch.full((1, self.tagset_size), 0.)
         init_alphas[0][self.tag_to_ix[START_TAG]] = 1.
 
-        # first scale
+        # initial scale
         scales = torch.full((1, feats.size(0) + 1), 0.)
         scales[0][0] = init_alphas.sum()
 
         # scale alphas to avoid underflow
         scaled_alphas = init_alphas / scales[0][0]
 
+        bsz, time, dim = feats.unsqueeze(0).size()
+
         # iterate through the sentence
         for t, feat in enumerate(feats):
 
-            # recursion variables at t
-            alphas_t = []
+            # calculate emission probabilities (the same regardless of the next tag)
+            emit_scores = feat.view(bsz, self.tagset_size).expand(1, self.tagset_size, self.tagset_size)
+            emit_scores = torch.transpose(emit_scores, 1, 2)
 
-            for next_tag in range(self.tagset_size):
+            # calculate transition probabilities
+            trans_scores = torch.exp(self.log_transitions).unsqueeze(0)
 
-                # emission score the same regardless of previous tag
-                emit_score = feat[next_tag].view(1, -1).expand(1, self.tagset_size)
+            # update alphas recursively
+            scaled_alphas = (scaled_alphas * trans_scores * torch.exp(emit_scores)).sum(2)
 
-                # transition score
-                trans_score = torch.exp(self.log_transitions[next_tag].view(1, -1))
-
-                # scaled recursion: delta_t(j) = c_t*alpha_hat_t(j) = sum_i (alpha_hat_t-1(i) * phi_t(j, i, x_t))
-                next_tag_var = torch.sum(scaled_alphas * trans_score * torch.exp(emit_score))
-
-                # save alpha for this tag
-                alphas_t.append(next_tag_var.view(1, -1))
-
-            # scale alphas again and save scale
-            scaled_alphas = torch.cat(alphas_t).view(1, -1)
+            # scale and save scale
             scale = scaled_alphas.sum()
             scales[0][t + 1] = scale
             scaled_alphas /= scale
 
-        # Z(x) = prod_t c_t (Bishop) -> log(Z(x)) = sum_t log(c_t) (partition func. is product of scales)
+        # partition func. is product of scales (Z(x) = prod_t c_t -> log(Z(x)) = sum_t log(c_t))
         return torch.sum(torch.log(scales))
 
     def _get_lstm_features(self, sentence):
@@ -100,44 +94,42 @@ class BiLSTM_ChainCRF(nn.Module):
         return score
 
     def _viterbi_decode(self, feats):
+
+        # initialize list to keep track of backpointers
         backpointers = []
 
-        # Initialize the viterbi variables in log space
+        # initialize the viterbi variables in log space
         init_vvars = torch.full((1, self.tagset_size), -10000.)
         init_vvars[0][self.tag_to_ix[START_TAG]] = 0
 
         # forward_var at step i holds the viterbi variables for step i-1
         forward_var = init_vvars
         for feat in feats:
-            bptrs_t = []  # holds the backpointers for this step
-            viterbivars_t = []  # holds the viterbi variables for this step
 
-            for next_tag in range(self.tagset_size):
-                # next_tag_var[i] holds the viterbi variable for tag i at the
-                # previous step, plus the score of transitioning
-                # from tag i to next_tag.
-                # We don't include the emission scores here because the max
-                # does not depend on them (we add them in below)
-                next_tag_var = forward_var + self.log_transitions[next_tag]
-                best_tag_id = argmax(next_tag_var)
-                bptrs_t.append(best_tag_id)
-                viterbivars_t.append(next_tag_var[0][best_tag_id].view(1))
-            # Now add in the emission scores, and assign forward_var to the set
+            # calculate viterbi vars
+            next_tag_vars = forward_var.unsqueeze(0) + self.log_transitions.unsqueeze(0)
+            _, idx = torch.max(next_tag_vars, 2)
+            best_tag_ids = idx
+            indices = torch.transpose(best_tag_ids.unsqueeze(0).repeat(1, self.tagset_size, 1), 1, 2)
+            viterbivars_t = torch.gather(next_tag_vars, 2, indices)[:, :, 0]
+
+            # add emission scores and assign forward_var to the set
             # of viterbi variables we just computed
-            forward_var = (torch.cat(viterbivars_t) + feat).view(1, -1)
-            backpointers.append(bptrs_t)
+            forward_var = (viterbivars_t + feat).view(1, -1)
+            backpointers.append(best_tag_ids[0].tolist())
 
-        # Transition to STOP_TAG
+        # transition to STOP_TAG
         terminal_var = forward_var + self.log_transitions[self.tag_to_ix[STOP_TAG]]
         best_tag_id = argmax(terminal_var)
         path_score = terminal_var[0][best_tag_id]
 
-        # Follow the back pointers to decode the best path.
+        # follow the back pointers to decode the best path
         best_path = [best_tag_id]
         for bptrs_t in reversed(backpointers):
             best_tag_id = bptrs_t[best_tag_id]
             best_path.append(best_tag_id)
-        # Pop off the start tag (we dont want to return that to the caller)
+
+        # pop off the start tag (we dont want to return that to the caller)
         start = best_path.pop()
         assert start == self.tag_to_ix[START_TAG]  # Sanity check
         best_path.reverse()
@@ -145,11 +137,11 @@ class BiLSTM_ChainCRF(nn.Module):
 
     def neg_log_likelihood(self, sentence, tags):
         feats = self._get_lstm_features(sentence)
-        forward_score = self._forward_alg(feats)
+        forward_score = self._forward_belief_prop(feats)
         gold_score = self._score_sentence(feats, tags)
         return forward_score - gold_score
 
-    def forward(self, sentence):  # dont confuse this with _forward_alg above.
+    def forward(self, sentence):  # dont confuse this with _forward_belief_prop above.
         # Get the emission scores from the BiLSTM
         lstm_feats = self._get_lstm_features(sentence)
 
